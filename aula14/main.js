c8c94c46bd92fc9ce8c06b2c1f0668061837d41b
Antônio Ricart <ricarteusuo11@gmail.com>
2024-10-01 11:22:11 -0300

    adiciona mais questões

--- aula14/main.js
+++ aula14/main.js
@@ -7,4 +7,12 @@ for (let i = 0; i<numeroElementos; i+=1){
     console.log(numero)
     numero += fator
 }
+// Operadores lógicos
+let bool = true && true // and
+bool = true || false  // or
+boll = !false // not
 
+// Modifique o programa acima para exibir apenas 
+// os números ímpares da PA que não sejam 
+// divisíveis por 5. Caso o numero e o fator sejam
+// pares, deve ser exibido uma mensagem de erro
