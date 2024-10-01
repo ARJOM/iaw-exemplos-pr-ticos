d08df6ed5a6d0e4c90ca7f88ccfebae06a850230
Antônio Ricart <ricarteusuo11@gmail.com>
2024-10-01 10:16:23 -0300

    adiciona exemplos de função

--- aula13/main.js
+++ aula13/main.js
@@ -76,9 +76,76 @@ console.log(arrayPreenchido)
 
 console.log(arrayPreenchido[1])
 
+
+// Constantes
+
+const NUMERO = 13
+console.log(NUMERO)
+// NUMERO = 17
+// console.log(NUMERO)
+
+// Funções e métodos
+
+// def nome_funcao():
+//     return "Olá"
+function nomeFuncao(){
+    return "Olá"
+}
+console.log(nomeFuncao())
+
+const nomeFuncao2 = () => {
+    return "Olá 2"
+}
+
+console.log(nomeFuncao2())
+
+
+function some(a, b){
+    return a+b
+}
+
+console.log("Soma:", some(1, 2))
+
+const values = [10,2]
+
+function multiplicaPorFator(valores, divisor=1){
+    valores.push((valores[0]* valores[1])/divisor)
+}
+multiplicaPorFator(values)
+console.log("Fator resulta em:", values[2])
+multiplicaPorFator(values, 3)
+console.log("Fator resulta em:", values[3])
+
+function primo(numero){
+    if (numero <= 1){
+        return false
+    }
+    let n = 2
+    while (n <= numero/2){
+        if(numero%n == 0){
+            return false
+        }
+        n+=1
+    }
+    return true
+}
+
+
+console.log("2 é primo?", primo(2))
+console.log("1 é primo?", primo(1))
+console.log("1 é primo?", primo(1))
+console.log("4 é primo?", primo(4))
+console.log("5 é primo?", primo(5))
+console.log("17 é primo?", primo(17))
+
+
 // Métodos de arrays
 for (let item of arrayPreenchido){
     console.log(item)
+}
+
+for (let key in {"nome": "Ricart", "idade": 24}){
+    console.log(key)
 }
 
 arrayPreenchido.forEach(item => {
