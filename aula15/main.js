bfa9b0682e168e9e88c34426dd058bf26c26404f
Antônio Ricart <ricarteusuo11@gmail.com>
2024-10-22 09:56:10 -0300

    Adiciona elementos de criação do DOM

--- aula15/main.js
+++ aula15/main.js
@@ -1,9 +1,11 @@
-let alunos = []
+let alunos = [{nome: "Ricart", nota:90}]
 
 // Captura de elementos
 
 let nome = document.getElementsByName("name")[0]
 let nota = document.getElementsByName("nota")[0]
+
+let conteudo = document.getElementById("content")
 
 let botao = document.getElementById("submit")
 
@@ -13,6 +15,31 @@ function imprimeElementos(){
     console.log(nome.value)
     console.log(nota.value)
 }
+
+function adicionaLinhaNaTabela(nome, nota){
+    // Criando elementos
+    const tr = document.createElement("tr")
+        
+    const td1 = document.createElement("td")
+    td1.classList.add("red")
+    // td1.className = "red"
+    // td1.setAttribute("class", "red")
+
+    td1.textContent = nome
+    const td2 = document.createElement("td")
+    td2.textContent = nota
+
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+
+    conteudo.appendChild(tr)
+}
+
+window.addEventListener("load", () => {
+    alunos.map(aluno => {
+        adicionaLinhaNaTabela(aluno.nome, aluno.nota)
+    })
+})
 
 botao.addEventListener("click", imprimeElementos)
 
@@ -27,9 +54,20 @@ function adicionaNota(){
         nota: notaAluno
     })
 
+     // Invoca função que altera o DOM
+
+     adicionaLinhaNaTabela(nomeAluno, notaAluno)
+
     // Apagar valores
     nota.value=''
     nome.value=''
 }
 
 botao.addEventListener("click", adicionaNota)
+
+
+// let dialog = document.getElementById('dialog')
+
+// dialog.open = false;
+
+// botao.addEventListener("click", () => dialog.open = !dialog.open)
