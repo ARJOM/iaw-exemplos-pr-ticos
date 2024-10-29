de71945924069dc3d1543bef2d088f1be497f09d
Antônio Ricart <ricarteusuo11@gmail.com>
2024-10-29 10:40:20 -0300

    adiciona local storage

--- aula17/main.js
+++ aula17/main.js
@@ -3,6 +3,17 @@ const inputBusca = document.getElementById("pesquisa")
 const conteudoTabela = document.getElementById("conteudo")
 
 let produtos = []
+if (window.localStorage){
+    produtos = getLocalStorage("produtos")
+}
+
+window.addEventListener("load", (() => {
+    produtos.forEach((produto) => {
+        let linha = criaLinha(produto.nome, produto.preco, produto.quantidade)
+        conteudoTabela.appendChild(linha)
+    })
+}))
+
 
 function criaLinha(nome, preco, quantidade){
     const linha = document.createElement("tr");
@@ -29,6 +40,13 @@ formularioRegistro.addEventListener("submit", (e)=>{
     let valorPreco = preco.value
     let quantidade = document.getElementById("productQuantity")
     let valorQuantidade = quantidade.value
+
+    produtos.push({
+        nome: valorNome,
+        preco: valorPreco,
+        quantidade: valorQuantidade
+    })
+    setLocalStorage('produtos', produtos)
     
     let linha = criaLinha(valorNome, valorPreco, valorQuantidade)
     conteudoTabela.appendChild(linha)
@@ -50,3 +68,11 @@ inputBusca.addEventListener("input", () => {
             
         })
 })  
+
+function setLocalStorage(chave, valor){
+    localStorage.setItem(chave, JSON.stringify(valor))
+}
+
+function getLocalStorage(chave){
+    return JSON.parse(localStorage.getItem(chave))
+}
